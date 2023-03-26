05f096f11eda27ffc743091839f72482bce3829f
afitzer <alexpfitzgerald@yahoo.com>
2023-03-26 12:04:03 -0500

    task 4

--- fitzforecast/forms.py
+++ fitzforecast/forms.py
@@ -5,4 +5,4 @@ class PlanningForm(forms.Form):
     product = forms.ModelChoiceField(queryset=Product.objects.all())
     customer = forms.ModelChoiceField(queryset=Customer.objects.all())
     date = forms.DateField()
-    gross_sales = forms.IntegerField(required=True, help_text='This is monthly gross sales planning at the product and customer combo.')
+    gross_sales = forms.IntegerField(required=True, help_text='This is monthly gross sales planning. Put the 1st day of the planning month.')
--- fitzforecast/views.py
+++ fitzforecast/views.py
@@ -1,6 +1,6 @@
 # import pandas as pd
-import json
-# from plotly.utils import PlotlyJSONEncoder
+# import json
+from plotly.utils import PlotlyJSONEncoder
 # import plotly.express as px
 # from django.http import JsonResponse
 from plotly.offline import plot
@@ -9,6 +9,11 @@ from django.shortcuts import render
 from .models import GrossSales, Product
 from .forms import PlanningForm
 from django.db.models import Sum
+from django.db.models.functions import TruncMonth
+from django.db.models import Q
+from datetime import datetime
+
+current_year = datetime.now().year
 
 def index(request):
     grosssales = GrossSales.objects.all()
@@ -22,20 +27,51 @@ def planning(request):
             customer = form.cleaned_data['customer']
             date = form.cleaned_data['date']
             gross_sales = form.cleaned_data['gross_sales']
-
             GrossSales.objects.create(product=product, customer=customer, date=date, gross_sales=gross_sales)
         else:
             print(form.errors)
+        form = PlanningForm()
         data = GrossSales.objects.all()
-        return render(request, 'fitzforecast/planning.html', {'form': form, 'data': data})
+        product_sales = Product.objects.annotate(
+            total_sales=Sum('grosssales__gross_sales'),
+            jan_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=1)),
+            feb_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=2)),
+            mar_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=3)),
+            apr_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=4)),
+            may_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=5)),
+            jun_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=6)),
+            jul_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=7)),
+            aug_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=8)),
+            sep_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=9)),
+            oct_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=10)),
+            nov_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=11)),
+            dec_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=12)),
+            ).values('description', 'total_sales', 'grosssales__customer__name', 'jan_sales', 'feb_sales', 'mar_sales', 'apr_sales', 'may_sales', 'jun_sales', 'jul_sales', 'aug_sales', 'sep_sales', 'oct_sales', 'nov_sales', 'dec_sales')
+        return render(request, 'fitzforecast/planning.html', {'form': form, 'data': data, 'product_sales': product_sales})
     else:
         form = PlanningForm()
         data = GrossSales.objects.all()
-        product_sales = Product.objects.annotate(total_sales=Sum('grosssales__gross_sales')).values('description', 'total_sales')
+        product_sales = Product.objects.annotate(
+            total_sales=Sum('grosssales__gross_sales'),
+            jan_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=1)),
+            feb_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=2)),
+            mar_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=3)),
+            apr_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=4)),
+            may_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=5)),
+            jun_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=6)),
+            jul_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=7)),
+            aug_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=8)),
+            sep_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=9)),
+            oct_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=10)),
+            nov_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=11)),
+            dec_sales=Sum('grosssales__gross_sales', filter=Q(grosssales__date__year=current_year, grosssales__date__month=12)),
+            ).values('description', 'total_sales', 'grosssales__customer__name', 'jan_sales', 'feb_sales', 'mar_sales', 'apr_sales', 'may_sales', 'jun_sales', 'jul_sales', 'aug_sales', 'sep_sales', 'oct_sales', 'nov_sales', 'dec_sales')
         return render(request, 'fitzforecast/planning.html', {'form': form, 'data': data, 'product_sales': product_sales})
     
 def reports(request):
-    product_sales = Product.objects.annotate(total_sales=Sum('grosssales__gross_sales')).values('description', 'total_sales')
+    product_sales = Product.objects.annotate(
+        total_sales=Sum('grosssales__gross_sales')
+        ).values('description', 'total_sales')
 
     data = [
         go.Bar(
@@ -44,8 +80,4 @@ def reports(request):
         )
     ]
     graphJSON = plot(data, output_type='json', auto_open=False)
-
-    context = {
-        'graphJSON': graphJSON
-    }
-    return render(request, 'fitzforecast/reports.html', context)
+    return render(request, 'fitzforecast/reports.html', {'graphJSON': graphJSON})
