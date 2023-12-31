6a35d7cfa235ff655b7d77d649227cc7d9af72fd
afitzer <alexpfitzgerald@yahoo.com>
2023-12-31 09:34:59 -0600

    Update date field in PlanningForm

--- fitzforecast/forms.py
+++ fitzforecast/forms.py
@@ -4,5 +4,5 @@ from .models import Product, Customer
 class PlanningForm(forms.Form):
     product = forms.ModelChoiceField(queryset=Product.objects.all())
     customer = forms.ModelChoiceField(queryset=Customer.objects.all())
-    date = forms.DateField()
+    date = forms.DateField(widget=forms.SelectDateWidget(empty_label=("Year", "Month", "Day")), initial="YYYY-MM-01")
     gross_sales = forms.IntegerField(required=True, help_text='This is monthly gross sales planning. Put the 1st day of the planning month.')
